8cc7f854778aa646cd0301a1bd855a629b546b04
Ian Marino <ian.marino.r@gmail.com>
2022-12-12 01:15:39 +0000

    1.3.12

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import {seed} from './module.js'
 
 let versaoHTML = document.getElementById('versao')
-let versao = 'Alpha 1.3.11'
+let versao = 'Alpha 1.3.12'
 versaoHTML.innerHTML = versao
 
 function showVersion() {
